b8c8fabd61bff9724698d6cdad86e8f707ecd85d
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-31 12:14:40 +0800

    fix bug: should ! checkers

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,59 @@
 'use strict';
+require('dotenv').config();
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
+const { setDebugging, logReq, logRes } = require('../log-utils');
+
+setDebugging(process.env.DEBUG == 'yes');
 
 module.exports = function (app) {
-  
+
   let solver = new SudokuSolver();
 
+  const noSutionE = 'Puzzle cannot be solved';
+
+  const noPuzzleE = 'Required field missing';
+
+  const missingFieldsE = "Required field(s) missing";
+
+
   app.route('/api/check')
-    .post((req, res) => {
+    .post(logReq('body'), logRes, (req, res) => {
+      const puzzle = req.body?.puzzle,
+        coordinate = req.body?.coordinate,
+        value = req.body?.value;
 
+      if (!puzzle || !coordinate || !value)
+        res.json({ error: missingFieldsE });
+      else {
+        try {
+          const conflict = [];
+          const [r, c] = solver.fromCoordToRC(coordinate);
+          !solver.checkRowPlacement(puzzle, r, c, value) && conflict.push('row');
+          !solver.checkColPlacement(puzzle, r, c, value) && conflict.push('column');
+          !solver.checkRegionPlacement(puzzle, r, c, value) && conflict.push('region');
+
+          if (conflict.length > 0)
+            res.json({ valid: false, conflict })
+          else
+            res.json({ valid: true });
+        } catch (e) {
+          res.json({ error: e });
+        }
+      }
     });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
 
+  app.route('/api/solve')
+    .post(logReq({ body: true }), logRes, (req, res) => {
+      const puzzle = req.body?.puzzle?.valueOf();
+      if (!puzzle || puzzle == '')
+        res.json({ error: noPuzzleE });
+      else {
+        try {
+          res.json({ solution: solver.solve(req.body.puzzle) });
+        } catch (e) {
+          res.json({ error: e });
+        }
+      }
     });
 };
