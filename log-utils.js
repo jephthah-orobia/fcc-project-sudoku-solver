f4ed50c8611c61d01e690006efba2932de1f92db
Jephthah Orobia <jephthah.orobia@r5-1.deped.gov.ph>
2022-12-31 12:02:46 +0800

    new middle wares: logReq() and logRes

--- log-utils.js
+++ log-utils.js
@@ -50,6 +50,69 @@ const logBody = (req, res, next) => {
     next();
 }
 
+/**
+ * A middleware generator that will log req object base on set propertyName or options.
+ * @param {String} propertyName the property of req to log.
+ * @param {Object}  options set option flase
+ * @description available options default: { declare = true, body = false, query = false, params = false, reqProps = []}
+ * @returns a middleware that logs req object based on set propertyName or options.
+ */
+function logReq({ declare = true, body = false, query = false, params = false, reqProps = [] }) {
+    if (typeof arguments[0] == 'string')
+        reqProps.push(arguments[0]);
+    else if (arguments[0] instanceof Array)
+        reqProps.push(...arguments[0]);
+    else if (!typeof arguments[0] == 'object')
+        throw TypeError("Only accepts a propertyName (string) or options (object) or listOfProperyNames[]");
+
+    for (let i = 1; i < arguments.length; i++)
+        if (typeof arguments[i] == 'string' || typeof arguments[i] == 'number')
+            reqProps.push(arguments[i]);
+
+    const declareReq = (req) => {
+        log('- - - - - - - - - - - - - - - - - - - - - - - - - - - -');
+        log(">> REQUEST: ", req.method, ":", req.originalUrl, "from", req.ip);
+    }
+    return (req, res, next) => {
+        declare && declareReq(req);
+        query && logPropsOf('query', req.query);
+        params && logPropsOf('params', req.params);
+        body && logPropsOf('body', req.body);
+        if (reqProps.length > 0)
+            for (let prop of reqProps)
+                logPropsOf(prop, req[prop]);
+        next();
+    }
+}
+
+/**
+ * A middleware for logging the response.
+ */
+function logRes(req, res, next) {
+    const sendInterceptor = (res, send) => (content) => {
+        res.content = content;
+        res.send = send;
+        res.send(content);
+    };
+
+    res.send = sendInterceptor(res, res.send);
+
+    const onFinish = (data) => {
+        log("<< RESPONSE: ");
+        console.group();
+        log(res.content);
+        console.groupEnd();
+        log('* * * * * * * * * * * * * * * * * * * * * * * * * * * *');
+        res.removeListener('finish', onFinish);
+        res.removeListener('close', onFinish);
+        next();
+    }
+
+    res.on('finish', onFinish);
+    res.on('close', onFinish);
+    next();
+}
+
 module.exports = {
     setDebugging,
     log,
@@ -58,5 +121,7 @@ module.exports = {
     logParams,
     logBody,
     logQuery,
-    logPropsOf
+    logPropsOf,
+    logReq,
+    logRes
 };
